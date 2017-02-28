c8ec9bf606f3db980391676b6a99c398b692e7b0
Elizabeth <dentdelion@yandex.ru>
2017-02-28 21:15:05 +0200

    linear regression

--- chm/linreg.py
+++ chm/linreg.py
@@ -6,6 +6,18 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
+def generate_wave_set(n_support=1000, n_train=25, std=0.3):
+    x = np.linspace(0, 10, num=n_support)
+
+    y = 2*np.exp(x) + 6
+    
+    x_train = np.sort(np.random.choice(x, size=n_train, replace=True))
+
+    y_train = 2*np.exp(x_train) + 6 + np.random.normal(0, std, size=x_train.shape[0])
+    return (x_train, y_train)
+
+# (x, y) = generate_wave_set();
+
 x = [1, 2, 3, 4, 5, 6]
 y = [6, 5, 8, 6, 8, 0]
 
@@ -16,10 +28,12 @@ w = np.dot(np.dot(np.linalg.inv(np.dot(X.T, X)), X.T), y)
 y_hat = np.dot(w, X.T)
 
 print y_hat
-plt.plot(x, y_hat, 'r')
-plt.plot(x, y, 'ro') 
+plt.plot(x, y_hat, 'r', label='fitted')
+plt.plot(x, y, 'go', label='data') 
 plt.axhline(0, color='black')
 plt.axvline(0, color='black')
 plt.xlim(-1, 10)
 plt.ylim(-1, 10)
+plt.title('Fitted linear regression')
+plt.legend(loc='upper right', prop={'size': 20})
 plt.show()
